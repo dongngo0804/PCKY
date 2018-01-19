cb11420a43cccdf2b09198dab3c552f5a468c0b9
dongngo0804 <dongngo0804@gmail.com>
2018-01-19 23:09:49 +0700

    Update frequency

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -3,5 +3,8 @@ class Rule < ApplicationRecord
   def total
     Rule.where(left: left).pluck(:count).inject(0){ |sum,x| sum + x }
   end
-  
+
+  def freq
+    (self.count / self.total.to_f).round(2)
+  end
 end
